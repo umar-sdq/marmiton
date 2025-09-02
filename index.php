6745493cd52dec28ef5fa6e54a16462be328f041
Umar Siddiqui <2377328@cmontmorency.qc.ca>
2025-09-02 19:19:25 -0400

    creation fichiers de base, BD ne fonctionne pas

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require 'Modele.php';
+try {
+    $recettes = getRecettes();
+    require 'vueAcceuil.php';
+}  catch (Exception $e) {
+    $msgErreur = $e->getMessage();
+}
---
+++ vueAcceuil.php
@@ -0,0 +1,26 @@
+<?php 
+$titre = 'Marmiton';
+echo " TEST acceuil"; 
+
+ob_start(); 
+?>
+<h1>Liste des recettes</h1>
+
+<?php foreach ($recettes as $recette): ?>
+    <article>
+        <header>
+            <a href="<?= "recette.php?id=" . $recette['id'] ?>">
+                <h1 class="titreRecette"><?= $recette['titre'] ?></h1>
+            </a>
+            <h3><?= $recette['description'] ?></h3>
+            <time><?= $recette['date_creation'] ?></time>, 
+            par utilisateur #<?= $recette['utilisateur_id'] ?>
+        </header>
+    </article>
+    <hr />
+<?php endforeach; ?>
+
+<?php 
+$contenu = ob_get_clean(); 
+// require 'gabarit.php'; 
+?>
